4c96af35b9a318931d5eebd55580a3812bd66999
Adhishreya P <42477290+Adhishreya@users.noreply.github.com>
2021-05-16 01:28:14 +0530

    url_parser to be added

--- server/readme.md
+++ server/readme.md
@@ -1,1 +1,3 @@
 https://www.npmjs.com/package/spotify-web-api-node
+
+https://youtu.be/Xcet6msf3eE?list=PLxJPzY9lVfo6kWBxwE7yClxBjNie3iBdb&t=965
--- server/server.js
+++ server/server.js
@@ -3,10 +3,21 @@ const SpotifyWebApi=require('spotify-web-api-node')
 const app=express();
 
 app.post('/login',(req,res)=>{
+    const code=req.body.code
     //generating credentials for acccesss
     var spotifyApi = new SpotifyWebApi({
         clientId: 'fcecfc72172e4cd267473117a17cbd4d',
         clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
         redirectUri: 'http://www.example.com/callback'
       });
+      spotifyApi.authorizationCodeGrant(code).then(data=>{
+          res.json({
+            accessToken:data.body.access_token,
+            refreshToken : data.body.refresh_token,
+            expiresIn:data.body.expires_in
+          })
+          
+
+      }).catch(()=>
+      res.sendStatus(400))
 })
